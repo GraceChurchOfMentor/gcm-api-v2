abe53059ee07a55e4940e47a96008d746f1125c5
Nick Bair <njbair@gmail.com>
2016-06-16 18:05:06 -0400

    add boxcast sample config

---
+++ config/boxcast-sample.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'apiEndpointUri'      => "https://api.boxcast.com",
+    'apiLoginEndpointUri' => "https://login.boxcast.com",
+    'apiClientId'         => "BOXCAST_API_CLIENT_ID",
+    'apiClientSecret'     => "BOXCAST_API_CLIENT_SECRET",
+    'accountUsername'     => "BOXCAST_ACCOUNT_USERNAME",
+    'accountPassword'     => "BOXCAST_ACCOUNT_PASSWORD",
+];
+
